52a1c133bf6b2a99196730eb69b72f566477627f
Jenise Swall <jswall@vcu.edu>
2011-10-21 00:00:00 +0000

    version 0.2

--- R/ragged_image.R
+++ R/ragged_image.R
@@ -26,6 +26,14 @@
 ##       adds ragged image to a pre-existing plot.        
 ##    col: Color range to use for the ragged image, with the first
 ##       color assigned to zlim[1] and last color assigned to zlim[2].
+##    xlab, ylab: The labels to assign to the x- and y-axes.  If not
+##       specified by the user, these default to the expressions the
+##       user named as parameters x and y.  This behavior is similar
+##       to that for image().
+##    plt.beyond.zlim: If TRUE (and if zlim is specified by the user),
+##       z values beyond the limits given in zlim are plotted.
+##       Values below zlim[1] are plotted in the same color as zlim[1];
+##       values above zlim[2] are plotted in the same color as zlim[2].
 ##    ...: Any additional parameters to be passed to image function,
 ##         if add=FALSE.
 ##
@@ -39,11 +47,22 @@
 ## REVISION HISTORY:
 ##   Prototype: Jenise Swall, 2011-03-01 (based on Jenise's function
 ##      image.irreg.xgrid).
+##
+##   2011-10-21 (JLS): When zlim is provided by the user and
+##      plt.beyond.zlim=TRUE, allow plotting of points which have z
+##      values outside the bounds given by zlim.  For z values
+##      exceeding zlim[2], use the color corresponding to zlim[2]; for
+##      z values below zlim[1], use the color for zlim[1].  Also,
+##      added explicit parameters xlab and ylab.  This makes it easier
+##      to test whether the user has provided xlab and ylab, and, if
+##      not, allows these labels to default to the expressions the
+##      user named as parameters x and y.  This is similar to the
+##      default behavior for image().
 ## ###########################################################
 ragged.image <- function(x, y, z, zlim=range(z, na.rm=TRUE), add=FALSE,
-                         col=heat.colors(12), ...){
+                         col=heat.colors(12), xlab, ylab,
+                         plt.beyond.zlim=FALSE, ...){
 
-  
   ## If any elements of x or y (location information) are missing,
   ## then exit the function.
   if ( sum(is.na(x)) > 0 )
@@ -52,6 +71,27 @@ ragged.image <- function(x, y, z, zlim=range(z, na.rm=TRUE), add=FALSE,
     stop("No missing values allowed in 'y'.")
   
 
+  ## If plt.beyond.zlim=TRUE and zlim is given, we set z values
+  ## which are greater than zlim[2] to equal zlim[2] and z values
+  ## which are less than zlim[1] to equal zlim[1].  This ensures that
+  ## these values are plotted, when they would otherwise be blank.  If
+  ## plt.beyond.zlim=TRUE and zlim is missing, then zlim will be
+  ## determined according to the total range of the z's and
+  ## plt.beyond.zlim can be set to FALSE.
+  if (plt.beyond.zlim){
+    if (missing(zlim)){
+      warning("plt.beyond.zlim=TRUE is not a valid option if zlim argument is not provided, changing to plt.beyond.zlim=FALSE")
+      plt.beyond.zlim <- FALSE
+    }
+    else{
+      ## Values lower than zlim[1] set to zlim[1].
+      z[z < zlim[1]] <- zlim[1]
+      ## Values higher than zlim[2] set to zlim[2].
+      z[z > zlim[2]] <- zlim[2]
+    }
+  }
+    
+  
   ## Construct a regular grid, making sure that it includes the
   ## complete range of x and y values in the original data.
   x.uniq <- sort(unique(x))
@@ -71,10 +111,19 @@ ragged.image <- function(x, y, z, zlim=range(z, na.rm=TRUE), add=FALSE,
 
   
   ## If add=TRUE, add the image to the existing plot.
-  if (!add)
-    image(x.uniq, y.uniq, z.surf.mat, zlim=zlim, col=col, ...)    
+  if (!add){
+
+    ## If the user doesn't specify xlab and ylab, assign to them
+    ## the expressions the user named as parameters x and y.  This
+    ## is similar to the default behavior for image().
+    if (missing("xlab"))
+      xlab <- deparse(substitute(x))
+    if (missing("ylab"))
+      ylab <- deparse(substitute(y))
+    image(x.uniq, y.uniq, z.surf.mat, zlim=zlim, col=col,
+          xlab=xlab, ylab=ylab, ...)
+  }
   ## Otherwise, this image begins a new plot.
   else
     image(x.uniq, y.uniq, z.surf.mat, zlim=zlim, col=col, add=TRUE)
-
 }
